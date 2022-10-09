1981060d9088bdc04ec6678e4ed98825717c6868
Caio Guimarães <caio23guimaraes@gmail.com>
2022-10-09 08:50:32 -0300

    Adiciona roteamento

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 import Koa from 'koa'
+import Router from '@koa/router'
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async ctx => {
+router.get('/', async ctx => {
     ctx.body = { ola: 'Mundo' };
 });
+
+router.get('/users', async ctx => {
+    ctx.body = { ola: 'Users' };
+});
+
+app.use(router.routes())
+app.use(router.allowedMethods())
 
 app.listen(3000);

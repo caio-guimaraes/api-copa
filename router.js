38108e90365dc3a142693905031efa6d234cace2
Caio Guimarães <caio23guimaraes@gmail.com>
2022-10-09 16:06:33 -0300

    Remove a listagem de usuários

--- router.js
+++ router.js
@@ -6,7 +6,6 @@ import * as games from './app/games/index.js';
 export const router = new Router();
 
 router.post('/users', users.create);
-router.get('/users', users.list);
 
 // router.get('/hunches', hunches.list);
 router.post('/hunches', hunches.create);

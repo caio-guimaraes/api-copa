aaa89bf19001c5a11074209a1a497e26e2987b3b
Caio Guimarães <caio23guimaraes@gmail.com>
2022-10-09 14:55:25 -0300

    Cria controllers para as requisições e utiliza elas no router

--- router.js
+++ router.js
@@ -1,23 +1,7 @@
-import Router from '@koa/router'
+import Router from '@koa/router';
+import * as users from './app/users/index.js';
 
 export const router = new Router();
 
-const users = []
-
-router.get('/', async ctx => {
-    ctx.body = { ola: 'Mundo' };
-});
-
-router.get('/users', async ctx => {
-    ctx.body = { ola: 'Users' };
-});
-
-router.post('/users', async ctx => {
-    const user = {
-        username: ctx.request.body.username
-    }
-
-    users.push(user)
-
-    ctx.body = user
-});
+router.post('/users', users.create);
+router.get('/users', users.list);

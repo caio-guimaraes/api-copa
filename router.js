dedacef19d2d67214be8aafd66c9a451fc33ad68
Caio Guimarães <caio23guimaraes@gmail.com>
2022-10-09 15:20:12 -0300

    Cria rotas para criar e listar os palpites

--- router.js
+++ router.js
@@ -1,7 +1,11 @@
 import Router from '@koa/router';
 import * as users from './app/users/index.js';
+import * as hunches from './app/hunches/index.js';
 
 export const router = new Router();
 
 router.post('/users', users.create);
 router.get('/users', users.list);
+
+router.get('/hunches', users.hunches);
+router.post('/hunches', hunches.create);

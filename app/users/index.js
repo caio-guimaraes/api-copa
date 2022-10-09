c67233c2dd305ddc9806b6b1d3f3fb10289c2699
Caio Guimarães <caio23guimaraes@gmail.com>
2022-10-09 16:39:06 -0300

    Adiciona jwt secret no .env e cria script para rodar a aplicação com leitura do .env

--- app/users/index.js
+++ app/users/index.js
@@ -48,7 +48,7 @@ export const login = async (ctx) => {
     const accessToken = jwt.sign({
         sub: user.id,
         name: user.name
-    }, "JWT_SECRET")
+    }, process.env.JWT_SECRET)
 
     ctx.body = {
         user: result,

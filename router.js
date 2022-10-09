f28a0f0d39b146a621be4254014d3613cbc97ffb
Caio Guimarães <caio23guimaraes@gmail.com>
2022-10-09 15:46:56 -0300

    Adiciona listagem de jogos

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 import Router from '@koa/router';
 import * as users from './app/users/index.js';
 import * as hunches from './app/hunches/index.js';
+import * as games from './app/games/index.js';
 
 export const router = new Router();
 
@@ -9,3 +10,5 @@ router.get('/users', users.list);
 
 // router.get('/hunches', hunches.list);
 router.post('/hunches', hunches.create);
+
+router.get('/games', games.list);

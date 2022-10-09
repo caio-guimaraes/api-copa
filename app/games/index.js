1b8052a29cee4189c37efff7e0b9eae1a543f2b3
Caio Guimarães <caio23guimaraes@gmail.com>
2022-10-09 15:53:36 -0300

    Adiciona filtro de data na listagem dos jogos

--- app/games/index.js
+++ app/games/index.js
@@ -1,10 +1,21 @@
 import { PrismaClient } from '@prisma/client'
+import { addDays, formatISO } from 'date-fns'
 
 const prisma = new PrismaClient()
 
 export const list = async (ctx) => {
+    const currentDate = ctx.request.query.gameTime
+
+    // Datas maiores ou iguais ao current date, se o currentDate existir, se não existir não adiciona a query
+    const where = currentDate ? {
+        gameTime: {
+            gte: currentDate,
+            lt: formatISO(addDays(new Date(currentDate), 1))
+        }
+    } : {}
+
     try {
-        const games = await prisma.game.findMany()
+        const games = await prisma.game.findMany({ where })
         ctx.body = games
         ctx.status = 200
     } catch (error) {

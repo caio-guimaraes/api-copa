87688bc06203851e213b2c512327f598f8b98b4f
Caio Guimarães <caio23guimaraes@gmail.com>
2022-10-09 16:51:24 -0300

    Adiciona verificação de autenticação na listagem dos jogos

--- app/games/index.js
+++ app/games/index.js
@@ -1,25 +1,40 @@
+import jwt from 'jsonwebtoken'
 import { PrismaClient } from '@prisma/client'
 import { addDays, formatISO } from 'date-fns'
 
 const prisma = new PrismaClient()
 
 export const list = async (ctx) => {
-    const currentDate = ctx.request.query.gameTime
+    if (!ctx.headers.authorization) {
+        ctx.status = 401
+        return
+    }
 
-    // Datas maiores ou iguais ao current date, se o currentDate existir, se não existir não adiciona a query
-    const where = currentDate ? {
-        gameTime: {
-            gte: currentDate,
-            lt: formatISO(addDays(new Date(currentDate), 1))
-        }
-    } : {}
+    const [type, token] = ctx.headers.authorization.split(" ")
 
     try {
-        const games = await prisma.game.findMany({ where })
-        ctx.body = games
-        ctx.status = 200
+        const data = jwt.verify(token, process.env.JWT_SECRET)
+
+        const currentDate = ctx.request.query.gameTime
+    
+        // Datas maiores ou iguais ao current date, se o currentDate existir, se não existir não adiciona a query
+        const where = currentDate ? {
+            gameTime: {
+                gte: currentDate,
+                lt: formatISO(addDays(new Date(currentDate), 1))
+            }
+        } : {}
+    
+        try {
+            const games = await prisma.game.findMany({ where })
+            ctx.body = games
+            ctx.status = 200
+        } catch (error) {
+            ctx.body = error
+            ctx.status = 500
+        }
     } catch (error) {
-        ctx.body = error
-        ctx.status = 500
+        ctx.status = 401
+        return
     }
 }
--- app/hunches/index.js
+++ app/hunches/index.js
@@ -4,46 +4,57 @@ import jwt from 'jsonwebtoken'
 const prisma = new PrismaClient()
 
 export const create = async (ctx) => {
-    const [type, token] = ctx.headers.authorization.split(" ")
-    const data = jwt.verify(token, process.env.JWT_SECRET)
-    
-    if (!ctx.request.body.homeTeamScore && !ctx.request.body.awayTeamScore) {
-        ctx.status = 500
+    if (!ctx.headers.authorization) {
+        ctx.status = 401
         return
     }
 
-    const userId = 'cl91o3mmg0002vk4ol1gfjos4'
-    const { gameId } = ctx.request.body
-    const homeTeamScore = parseInt(ctx.request.body.homeTeamScore)
-    const awayTeamScore = parseInt(ctx.request.body.awayTeamScore)
-
-    // Update se já existir um gameId com o mesmo userId e insert se não houver
+    const [type, token] = ctx.headers.authorization.split(" ")
+    
     try {
-        const [hunch] = await prisma.hunch.findMany({
-            where: { userId, gameId },
-        })
-
-        if (hunch) {
-            ctx.body = await prisma.hunch.update({ 
-                where: { id: hunch.id },
-                data: {
-                    homeTeamScore, awayTeamScore
-                }
-            })
-        } else {
-            ctx.body = await prisma.hunch.create({ 
-                data: {
-                    userId,
-                    gameId,
-                    homeTeamScore,
-                    awayTeamScore
-                }
-            })
+        const data = jwt.verify(token, process.env.JWT_SECRET)
+    
+        if (!ctx.request.body.homeTeamScore && !ctx.request.body.awayTeamScore) {
+            ctx.status = 500
+            return
         }
 
-        ctx.status = 200
+        const userId = data.sub
+        const { gameId } = ctx.request.body
+        const homeTeamScore = parseInt(ctx.request.body.homeTeamScore)
+        const awayTeamScore = parseInt(ctx.request.body.awayTeamScore)
+
+        // Update se já existir um gameId com o mesmo userId e insert se não houver
+        try {
+            const [hunch] = await prisma.hunch.findMany({
+                where: { userId, gameId },
+            })
+
+            if (hunch) {
+                ctx.body = await prisma.hunch.update({ 
+                    where: { id: hunch.id },
+                    data: {
+                        homeTeamScore, awayTeamScore
+                    }
+                })
+            } else {
+                ctx.body = await prisma.hunch.create({ 
+                    data: {
+                        userId,
+                        gameId,
+                        homeTeamScore,
+                        awayTeamScore
+                    }
+                })
+            }
+
+            ctx.status = 200
+        } catch (error) {
+            ctx.body = error
+            ctx.status = 500
+        }
     } catch (error) {
-        ctx.body = error
-        ctx.status = 500
+        ctx.status = 401
+        return
     }
 }
